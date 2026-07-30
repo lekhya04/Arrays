47436e62d30caa1f2df3015a8fc7689d1c6918b6
Lekhya <lekhya1854@gmail.com>
2026-07-30 06:46:58 +0530

    Add implementation for Two Sum problem with user input handling

---
+++ TwoSum.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the target: ");
+        int target = sc.nextInt();
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("Indices: " + result[0] + " " + result[1]);
+
+        sc.close();
+    }
+}
